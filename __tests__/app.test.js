f45a6b3565f26ccae68e85cdca6f906bbd56f18b
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-10 15:27:34 -0600

    first login test failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,24 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+
+describe('oauth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('shoulld login and redirect users to /api/v1/github/dashboard', async () => {
+    const res = await (await request.agent(app).get('api/v1/github/callback?code=42')).redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'im_not_real',
+      email: 'fake_person@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
   afterAll(() => {
     pool.end();
